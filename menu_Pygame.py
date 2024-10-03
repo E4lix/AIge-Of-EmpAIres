10bae578d0c9df526ab757d0bc2dec339549347d
E4lix <Ealix95@gmail.com>
2024-10-03 16:04:55 +0200

    Ajout du menu de gestion des paramètres

--- menu_Pygame.py
+++ menu_Pygame.py
@@ -1,23 +1,42 @@
 import pygame
 import pygame_menu
+import pygame_menu.themes
+
+# Définitions des constantes
+HAUTEUR_PAGE = 650
+LARGEUR_PAGE = 900
 
 pygame.init()
-surface = pygame.display.set_mode((600, 400))
+surface = pygame.display.set_mode((LARGEUR_PAGE, HAUTEUR_PAGE))
 
-def set_difficulty(value, difficulty):
+def ChoisirDifficulte(value, difficulty):
     # Do the job here !
     pass
 
-def start_the_game():
+def LancerUnePartie():
     # Do the job here !
     pass
 
-menu = pygame_menu.Menu('Welcome', 400, 300,
+def ChoisirNbrBots(Number, Value):
+    # Choix du nombre de bots
+    pass
+
+def ParametreDeJeu():
+    # Paramètrages de la partie
+    pass
+
+menu = pygame_menu.Menu('Welcome', LARGEUR_PAGE, HAUTEUR_PAGE,
                        theme=pygame_menu.themes.THEME_BLUE)
 
-menu.add.text_input('Name :', default='John Doe')
-menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-menu.add.button('Play', start_the_game)
-menu.add.button('Quit', pygame_menu.events.EXIT)
+parametre_menu = pygame_menu.Menu('Paramètres', LARGEUR_PAGE, HAUTEUR_PAGE,
+                                  theme=pygame_menu.themes.THEME_DARK)
+
+menu.add.text_input('Name : ', default='Vincent Hugo')
+menu.add.button('Jouer', LancerUnePartie)
+menu.add.button('Paramètres', parametre_menu)
+menu.add.button('Quitter', pygame_menu.events.EXIT)
+
+parametre_menu.add.selector('Nombres de Joueurs : ', [('1', 1), ('2', 2)], onchange=ChoisirNbrBots)
+parametre_menu.add.selector('Difficulty : ', [('Hard', 1), ('Easy', 2)], onchange=ChoisirDifficulte)
 
 menu.mainloop(surface)
