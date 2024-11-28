dfb2701ae01cceb5980d6ce210dc603638e08477
E4lix <Ealix95@gmail.com>
2024-11-28 10:29:22 +0100

    Commentaire explicatif et validation de la méthode

--- Graphics/Tileset.py
+++ Graphics/Tileset.py
@@ -3,14 +3,14 @@ import pygame
 # Chargement de Tileset
 class Tileset:
     def __init__(self, file, size, margin, spacing):
-        self.file = file
-        self.size = size
-        self.margin = margin
-        self.spacing = spacing
-        self.image = pygame.image.load(file)
-        self.rect = self.image.get_rect()
-        self.tiles = []
-        self.load()
+        self.file = file # Fichier du tileset
+        self.size = size # Taille du tileset complet
+        self.margin = margin # Marge externe entre les tiles
+        self.spacing = spacing # Marge interne entre les tiles
+        self.image = pygame.image.load(file) # Charge l'image dans pygame
+        self.rect = self.image.get_rect() # Renvoie un rectangle correspondant aux dimensions et à la position de l'image
+        self.tiles = [] # Liste des tiles du tileset
+        self.load() # Charge le tileset
 
     def load(self):
         self.tiles = []
@@ -19,6 +19,8 @@ class Tileset:
         dx = self.size[0] + self.spacing
         dy = self.size[1] + self.spacing
 
+        # On parcourt le tileset colonnes par colonnes, lignes par lignes.
+        # Les tiles sont donc indexées de haut en bas puis de gauche à droite.
         for x in range(x0, w, dx):
             for y in range(y0, h, dy):
                 tile = pygame.Surface(self.size, pygame.SRCALPHA) # Paramètres SRCALPHA pour transparence du fond des tiles
